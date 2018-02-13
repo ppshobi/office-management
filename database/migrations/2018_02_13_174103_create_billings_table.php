ee1f3fea2f6186fae485c18fd63b2db7e77be39c
Shobi P P <ppshobi@gmail.com>
2018-02-13 23:14:49 +0530

    migrations

---
+++ database/migrations/2018_02_13_174103_create_billings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('amount');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('transaction_type_id');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}

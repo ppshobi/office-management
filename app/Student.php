aaa1f6e0737a3e4bd44eeb53f762b4d051329cd6
Shobi P P <ppshobi@gmail.com>
2018-02-18 16:52:42 +0530

    setup morphed Relationships between Transactions <=> Student,Staff

--- app/Student.php
+++ app/Student.php
@@ -15,6 +15,6 @@ class Student extends Model
 
     public function transactions()
     {
-        $this->hasMany('App\Transaction');
+        return $this->morphMany('App\Transaction', 'transactionable');
     }
 }

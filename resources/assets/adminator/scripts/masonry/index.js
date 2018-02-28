d645d43bc5cc0b3af7a28935b2773bcc1765b099
Shobi P P <ppshobi@gmail.com>
2018-02-28 22:35:58 +0530

    installing adminator

---
+++ resources/assets/adminator/scripts/masonry/index.js
@@ -0,0 +1,17 @@
+import * as $ from 'jquery';
+import Masonry from 'masonry-layout';
+
+export default (function () {
+    window.addEventListener('load', () = > {
+        if($('.masonry').length > 0
+)
+    {
+        new Masonry('.masonry', {
+            itemSelector: '.masonry-item',
+            columnWidth: '.masonry-sizer',
+            percentPosition: true,
+        });
+    }
+})
+    ;
+}());
---
+++ resources/assets/adminator/scripts/search/index.js
@@ -0,0 +1,12 @@
+import * as $ from 'jquery';
+
+export default (function () {
+    $('.search-toggle').on('click', e = > {
+        $('.search-box, .search-input'
+).
+    toggleClass('active');
+    $('.search-input input').focus();
+    e.preventDefault();
+})
+    ;
+}());

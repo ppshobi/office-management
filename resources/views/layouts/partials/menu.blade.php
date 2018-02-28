d1fa0397c76188fbd73bcebc34bd451fdeaf1778
Shobi P P <ppshobi@gmail.com>
2018-03-01 00:20:00 +0530

    cleanup and menu items update

--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -30,13 +30,83 @@
         <!-- ### $Sidebar Menu ### -->
         <ul class="sidebar-menu scrollable pos-r">
             <li class="nav-item mT-30 active">
-                <a class="sidebar-link" href="index.html">
+                <a class="sidebar-link" href="/dashboard">
                 <span class="icon-holder">
                   <i class="c-blue-500 ti-home"></i>
                 </span>
                     <span class="title">Dashboard</span>
                 </a>
             </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                <span class="icon-holder">
+                  <i class="c-teal-500  ti-book"></i>
+                </span>
+                    <span class="title">Course</span>
+                    <span class="arrow">
+                        <i class="ti-angle-right"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="/course">
+                            <span>Manage Courses</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="/course/create">
+                            <span>Add a Course</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-teal-500  ti-user"></i></span>
+                    <span class="title">Students</span>
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="/student"><span>Manage Students</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="/student/create"><span>Add a Student</span></a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-teal-500  ti-user"></i></span>
+                    <span class="title">Staffs</span>
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="/staff"><span>Manage Staffs</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="/staff/create"><span>Add a Staff</span></a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder"><i class="c-teal-500  ti-money"></i></span>
+                    <span class="title">Transactions</span>
+                    <span class="arrow"><i class="ti-angle-right"></i></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="/transaction"><span>View Transactions</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="/transaction/create"><span>Add a Transaction</span></a>
+                    </li>
+                </ul>
+            </li>
+
             <li class="nav-item">
                 <a class='sidebar-link' href="email.html">
                 <span class="icon-holder">
